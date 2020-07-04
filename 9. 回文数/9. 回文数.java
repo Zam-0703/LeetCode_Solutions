c177499ea8de4844b95bfe8d92cda08c19ca8886
Zam-0703 <Zam-0703@outlook.com>
2020-07-04 08:35:46 +0800

    add 9.回文数
    add 9.回文数

---
+++ 9. 回文数/9. 回文数.java
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode.cn id=9 lang=java
+ *
+ * [9] 回文数
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0 || x % 10 == 0 && x != 0){
+            return false;
+        }
+        int revnumber = 0;
+        while(x > revnumber){
+            revnumber = revnumber * 10 + x % 10;
+            x = x/10;
+        }
+        return x == revnumber || x == revnumber / 10;
+    }
+}
+// @lc code=end
+
